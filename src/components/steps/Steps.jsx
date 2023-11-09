c3e8d775626896bd2ab591948807f24c4374c29e
josephmusya <muciajoe@gmail.com>
2023-11-10 01:18:09 +0300

    Modified component elements

---
+++ src/components/steps/Steps.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './steps.css';
+
+const Steps = () => {
+    return (
+        <div className='steps'>
+            <div>
+                <strong>Step 1</strong>
+            </div>
+            <div>
+                <strong>Step 2</strong>
+            </div>
+            <div>
+                <strong>Step 3</strong>
+            </div>            
+        </div>
+    );
+}
+
+export default Steps;
--- src/pages/landing/body/Body.jsx
+++ src/pages/landing/body/Body.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './body.css';
+import Steps from '../../../components/steps/Steps';
 
 const Body = () => {
     return (
@@ -13,11 +14,18 @@ const Body = () => {
                     <article>
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. At facilis autem provident?
                     </article>
+                    <button className='hire-us'>Hire Us</button>
                 </div>
-                <div className='anchor-image'>
-                    <img src="https://img.freepik.com/free-vector/studying-concept-illustration_114360-1301.jpg?w=740&t=st=1699562478~exp=1699563078~hmac=f7e370e5ddf75e11e9d7d411932c595f3642961775fad204b6be5a17ed4f9dbe" alt="" />
-                </div>
+                <img src="https://img.freepik.com/free-vector/studying-concept-illustration_114360-1301.jpg?w=740&t=st=1699562478~exp=1699563078~hmac=f7e370e5ddf75e11e9d7d411932c595f3642961775fad204b6be5a17ed4f9dbe" alt="" />
             </div>
+            <h1>Steps</h1>
+            <Steps />
+            <div className='promo'>
+                <h2>Get a complete paper from as low as <span>$10.00</span></h2>
+                <button>Order Now</button>
+            </div>
+            <br />
+            <br />
         </div>
     );
 }
