3c5356e6c46ac965afdf334f975371617bb6492c
josephmusya <muciajoe@gmail.com>
2023-11-14 12:52:40 +0300

    Added browser router

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,33 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Body from './pages/landing/body/Body';
 import Footer from './components/footer/Footer';
-
+import { Route, Routes } from 'react-router-dom';
 function App() {
 
   const BodyContent = () => {
     return (
       <>
+        <Navbar />
         <Body />
+        <Footer />
+      </>
+    )
+  }
+
+  const Main = ()  => {
+    return (
+      <>
+        <div>Main</div>
       </>
     )
   }
 
   return (
     <>
-      <Navbar />
-      <BodyContent />
-      <Footer />
+      <Routes>
+        <Route exact path='/' element={<BodyContent/>}/>
+        <Route path='/app' element={<Main/>}/>
+      </Routes>
     </>
   )
 }
