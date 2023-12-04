ea07272f4fd73d81a0f8b55a81a9ab77ed8126e2
josephmusya <muciajoe@gmail.com>
2023-12-04 23:55:31 +0300

    Added notification read status handle

--- src/pages/main/notification/Notification.jsx
+++ src/pages/main/notification/Notification.jsx
@@ -14,9 +14,10 @@ const Notification = () => {
     const { userToken } = useAuthContext();
 
     const [notifications, setNotifications] = useState([]);
+
+    const notifUrl = `${import.meta.env.VITE_API_URL}/notifications`;
     
     const getNotifications = async(token) =>{
-        const notifUrl = `${import.meta.env.VITE_API_URL}/notifications`;
 
         const getNotif = await fetch(notifUrl, {
             method:'get',
@@ -30,8 +31,34 @@ const Notification = () => {
         setNotifications(notifications);
     }
 
-    const navigateToOrder = (orderId) => {
+    const navigateToOrder = (orderId, notifId) => {
         navigate(`../order/${orderId}`);
+        markNotificationRead(notifId)
+        .then((status)=>{
+            if (status===200){
+                console.log("Notification read");
+            }
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+    }
+
+    const markNotificationRead  = async(notifId) => {
+        const readNotification = await fetch(`${notifUrl}/${notifId}/`, {
+            method:'put',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${userToken}`
+            },
+            body:JSON.stringify({
+                'read_status':true
+            })
+        })
+
+        const status = readNotification.status;
+
+        return status;
     }
 
     useEffect(()=>{
@@ -43,13 +70,22 @@ const Notification = () => {
             {
                 notifications.map((notification, index)=>{
                     return (
-                        <div onClick={()=>navigateToOrder(notification.order_id)} className='notif-box' key={index}>
+                        <div style={{backgroundColor:notification?.read_status?'#eeeeee':''}} onClick={()=>navigateToOrder(notification.order_id, notification.id)} className='notif-box' key={index}>
                             <IoMdNotificationsOutline size={30}/>
                             <div className='notif-message'>
                                 <article>{notification.message}</article>
                             </div>  
                             <div className='notif-duration'>
-                                <article>{timeAgo(notification.created_at)}</article>                                  
+                                <article>{timeAgo(notification.created_at)}</article>        
+                                {
+                                    !notification?.read_status &&
+                                    <div style={{
+                                    backgroundColor:'green',
+                                    width:'10px',
+                                    height:'10px',
+                                    borderRadius:'50%'
+                                }}></div> 
+                                }                         
                             </div>
                         </div>
                         )
