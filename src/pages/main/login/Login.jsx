daaee15ef0cc843acfd5a625253ae59f7ff1a224
josephmusya <muciajoe@gmail.com>
2023-12-05 22:58:00 +0300

    Modified login screen: Add loading status indicator

--- src/pages/main/login/Login.jsx
+++ src/pages/main/login/Login.jsx
@@ -1,22 +1,64 @@
-import React from 'react';
+import React  from 'react';
 import './login.css';
 import { useAuthContext } from '../../../providers/AuthProvider';
+import { IoLogoIonic } from "react-icons/io";
+import { FiUser } from "react-icons/fi";
+import { MdLock } from "react-icons/md";
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
+import { useState, CSSProperties } from 'react';
+import PulseLoader from "react-spinners/PulseLoader";
+
+// import { IoMdEye } from "react-icons/md";
+// import { IoMdEyeOff } from "react-icons/md";
+
+
 const Login = () => {
-    
-    const { handleLogin } = useAuthContext();
+    const iconSize = 30;
+    const { handleLogin, loading } = useAuthContext();
+    const [visible, setVisible] = useState(false);
+    const style = {
+        height:'1rem'
+    }
+    const togglePassword = ()=> {
+        setVisible(!visible);
+    }
+
     return (
         <div className='login'>
-            <form onSubmit={handleLogin} className='login-form'>
-                <div>
-                    <input required id='username' type="text" placeholder='username' />                    
-                </div>
-                <div>
-                    <input required id='password' type="password" placeholder='password' />                    
-                </div>
-                <div>
-                    <button>Submit</button>
-                </div>
-            </form>
+            <div className='login-box'>
+                <IoLogoIonic size={120}/>
+                <h1>Flexypro</h1>
+                <article>Welcome back!</article>
+                <form onSubmit={handleLogin} className='login-form'>                    
+                    <div className='login-content'>
+                        <FiUser className='username-icon' size={iconSize}/>
+                        <input required id='username' type="text" placeholder='Username' />                    
+                    </div>
+                    <div className='login-content'>
+                        <MdLock className='password-icon' size={iconSize}/>
+                        <input required id='password' type={visible?"text":"password" } placeholder='Password' />  
+                        {
+                            visible?
+                            <IoEye onClick={togglePassword} className='password-icon-eye' size={20} />:
+                            <IoEyeOff onClick={togglePassword} className='password-icon-eye' size={20} />                                              
+                        }
+                    </div>
+                    <div className='submit-credentials'>                        
+                        <button>
+                            {
+                                loading?
+                                <PulseLoader color='#fff' size={10}  />:
+                                'ENTER'
+                            }
+                            
+                        </button>
+                    </div>
+                    <div className='register-prompt'>
+                        <article>Do not have an account? <span>Register</span></article>
+                    </div>
+                </form>
+            </div>            
         </div>
     );
 }
