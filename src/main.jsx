502a330698f55f0d979441f391d9b80e1e7acadd
josephmusya <muciajoe@gmail.com>
2023-12-11 21:06:05 +0300

    Add: Chat provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './providers/AuthProvider.jsx'
 import { OrderProvider } from './providers/OrderProvider.jsx'
-import { NotificationProvider } from './providers/NotificationProvider.jsx'
+import { ChatProvider } from './providers/ChatProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthProvider>
   <OrderProvider>
-  <NotificationProvider>
+  <ChatProvider>
     <App />
-  </NotificationProvider>    
+  </ChatProvider>
   </OrderProvider>
   </AuthProvider>
   </BrowserRouter>
