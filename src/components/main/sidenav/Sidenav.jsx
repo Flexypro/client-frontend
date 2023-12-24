84abca3c01c6f162e6a4f74e437ab028803e0155
josephmusya <muciajoe@gmail.com>
2023-12-24 15:32:20 +0300

    fix: Fixed styling

--- src/components/main/sidenav/Sidenav.jsx
+++ src/components/main/sidenav/Sidenav.jsx
@@ -44,73 +44,71 @@ const Sidenav = () => {
     }
 
     return (
-        <div>
-            <div className='top-nav'>
-                <h1 style={{cursor:'pointer'}} className='heading-logo'  onClick={()=>navigate('./')}>
-                    <img src={gigitise} style={{width:'3rem'}} alt="" />
-                    Gigitise</h1>
-                <div className='search-nav'>
-                    <input value={searchQuery} onChange={(e)=>searchOrdersFromQuery(e.target.value)} type="text" placeholder='Search my orders' />
+        <div className='top-nav'>
+            <h1 style={{cursor:'pointer'}} className='heading-logo'  onClick={()=>navigate('./')}>
+                <img src={gigitise} style={{width:'3rem'}} alt="" />
+                Gigitise</h1>
+            <div className='search-nav'>
+                <input value={searchQuery} onChange={(e)=>searchOrdersFromQuery(e.target.value)} type="text" placeholder='Search my orders' />
+                {
+                    (suggestions.length > 0 && searchQuery) && 
+                    <div className='suggestions'>
+                        {
+                            suggestions?.map((suggestedOrder, index)=>{
+                                return(
+                                    <div className='suggested' key={index} onClick={()=>goToOrder(suggestedOrder.id)}>
+                                        <article>{suggestedOrder.title}</article>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                }
+            </div>                
+            <div className='add-task' onClick={()=>navigate('./create-task')}>
+                <MdAdd className='add-icon' size={iconSize}/>
+                <article>Create a new task</article>
+            </div>                               
+            <div className='profile'>
+                <article className='logout' onClick={()=>handleLogOut()}>
+                    Logout
+                </article> 
+                <div className="help">
+                    <MdHelpOutline className='' size={iconSize}/>
+                </div>
+                <div className='notif-bell' style={{cursor:'pointer'}} onClick={()=>navigate('./notifications')} >
+                    <IoMdNotificationsOutline className='notif-icon'  size={iconSize}/>
                     {
-                        (suggestions.length > 0 && searchQuery) && 
-                        <div className='suggestions'>
-                            {
-                                suggestions?.map((suggestedOrder, index)=>{
-                                    return(
-                                        <div className='suggested' key={index} onClick={()=>goToOrder(suggestedOrder.id)}>
-                                            <article>{suggestedOrder.title}</article>
-                                        </div>
-                                    )
-                                })
-                            }
+                        unreadNotif.length > 0 &&
+                        <div>
+                            <article>
+                                {
+                                    unreadNotif.length > 9?
+                                    '9+':
+                                    unreadNotif.length
+                                }
+                            </article>
                         </div>
                     }
-                </div>                
-                <div className='add-task' onClick={()=>navigate('./create-task')}>
-                    <MdAdd className='add-icon' size={iconSize}/>
-                    <article>Create a new task</article>
-                </div>                               
-                <div className='profile'>
-                    <article className='logout' onClick={()=>handleLogOut()}>
-                        Logout
-                    </article> 
-                    <div className="help">
-                        <MdHelpOutline className='' size={iconSize}/>
-                    </div>
-                    <div className='notif-bell' style={{cursor:'pointer'}} onClick={()=>navigate('./notifications')} >
-                        <IoMdNotificationsOutline className='notif-icon'  size={iconSize}/>
-                        {
-                            unreadNotif.length > 0 &&
-                            <div>
-                                <article>
-                                    {
-                                        unreadNotif.length > 9?
-                                        '9+':
-                                        unreadNotif.length
-                                    }
-                                </article>
-                            </div>
-                        }
-                    </div>                    
-                    <div className='settings'>
-                        <IoMdSettings onClick={()=>navigate('./settings')} style={{cursor:'pointer'}} size={iconSize}/>
-                    </div>
-                    <div className='profile-info' onClick={()=>navigate('./profile')}>
-                        <article className={loadingUserProfile?'username-skeleton':''} style={{width: loadingUserProfile?'3rem':''}}>{userProfile?.username}</article>
-                        {
-                            userProfile?.profile_photo?
-                            <img style={{
-                                animation: loadingUserProfile?`skeleton-loading 1s linear infinite alternate`:''
-                            }} src={userProfile?.profile_photo} alt="profile cover" />:
-                            <article style={{
-                                animation: loadingUserProfile?`skeleton-loading 1s linear infinite alternate`:''
-
-                            }} className='img-placeholder' >{userProfile && `${(userProfile?.username?.charAt(0)?.toUpperCase() + userProfile?.username.slice(1).slice(0,1))}`}</article>
-                        }
-                    </div>
+                </div>                    
+                <div className='settings'>
+                    <IoMdSettings onClick={()=>navigate('./settings')} style={{cursor:'pointer'}} size={iconSize}/>
                 </div>
-            </div>        
-        </div>
+                <div className='profile-info' onClick={()=>navigate('./profile')}>
+                    <article className={loadingUserProfile?'username-skeleton':''} style={{width: loadingUserProfile?'3rem':''}}>{userProfile?.username}</article>
+                    {
+                        userProfile?.profile_photo?
+                        <img style={{
+                            animation: loadingUserProfile?`skeleton-loading 1s linear infinite alternate`:''
+                        }} src={userProfile?.profile_photo} alt="profile cover" />:
+                        <article style={{
+                            animation: loadingUserProfile?`skeleton-loading 1s linear infinite alternate`:''
+
+                        }} className='img-placeholder' >{userProfile && `${(userProfile?.username?.charAt(0)?.toUpperCase() + userProfile?.username.slice(1).slice(0,1))}`}</article>
+                    }
+                </div>
+            </div>
+        </div>        
     );
 }
 
