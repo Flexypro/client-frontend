50eb982b334f06f644b1eb3a9fe3698887dd78bf
josephmusya <muciajoe@gmail.com>
2024-01-29 23:01:48 +0300

    Add: responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,74 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/main/dashboard/Dashboard';
-import Notification from './pages/main/notification/Notification';
-import OrderCreation from './pages/main/orders/order-creation/OrderCreation';
-import Profile from './pages/main/profile/Profile';
-import Settings from './pages/main/settings/Settings';
-import OrderView from './pages/main/orders/order-view/OrderView';
-import Login from './pages/main/login/Login';
-import { useAuthContext } from './providers/AuthProvider';
-import Register from './pages/main/register/Register';
-import PasswordReset from './pages/main/reset-password/PasswordReset';
-import SetPassword from './components/main/modal/set-password/SetPassword';
-import BadToken from './pages/main/bad-token/BadToken';
-import ExpiredToken from './pages/main/expired-token/ExpiredToken';
-import ActivateAccount from './components/main/modal/set-password/activate-account/ActivateAccount';
-import { useState } from 'react';
-import Navbar from './components/main/navbar/Navbar';
-import SideNav from './components/main/sidenav/SideNav';
-import Completed from './pages/main/orders/completed/Completed';
-import InProgress from './pages/main/orders/in-progress/InProgress';
-import Available from './pages/main/orders/available/Available';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/main/dashboard/Dashboard";
+import Notification from "./pages/main/notification/Notification";
+import OrderCreation from "./pages/main/orders/order-creation/OrderCreation";
+import Profile from "./pages/main/profile/Profile";
+import Settings from "./pages/main/settings/Settings";
+import OrderView from "./pages/main/orders/order-view/OrderView";
+import Login from "./pages/main/login/Login";
+import { useAuthContext } from "./providers/AuthProvider";
+import Register from "./pages/main/register/Register";
+import PasswordReset from "./pages/main/reset-password/PasswordReset";
+import SetPassword from "./components/main/modal/set-password/SetPassword";
+import BadToken from "./pages/main/bad-token/BadToken";
+import ExpiredToken from "./pages/main/expired-token/ExpiredToken";
+import ActivateAccount from "./components/main/modal/set-password/activate-account/ActivateAccount";
+import { useState } from "react";
+import Navbar from "./components/main/navbar/Navbar";
+import SideNav from "./components/main/sidenav/SideNav";
+import Completed from "./pages/main/orders/completed/Completed";
+import InProgress from "./pages/main/orders/in-progress/InProgress";
+import Available from "./pages/main/orders/available/Available";
 
 function App() {
   const { userToken, loadedUserProfile } = useAuthContext();
 
-  const Main = ()  => {
-
+  const Main = () => {
     return (
       <>
         <main className="app">
           <SideNav />
-          <div className='app-main-content'>
+          <div className="app-main-content">
             <Navbar />
-            <div className='routes'>
+            <div className="routes">
               <Routes>
-                <Route path='/' element={<Dashboard />}/> 
-                <Route path='/available' element={<Available/>} />
-                <Route path='/in-progress' element={<InProgress/>}/> 
-                <Route path='/completed'element={<Completed/>}/>
-                <Route path='/notifications' element={<Notification/>}/>
-                <Route path='/create-task' element={<OrderCreation/>}/>        
-                <Route path='/profile' element={<Profile/>}/>       
-                <Route path='/settings' element={<Settings/>}/> 
-                <Route path='/order/:orderId' element={<OrderView/>}/>
-              </Routes> 
-            </div>   
-          </div>                  
-        </main> 
-        {
-          loadedUserProfile?.is_verified === 'False' && <ActivateAccount token={userToken} email={loadedUserProfile?.email}/>
-        }       
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/available" element={<Available />} />
+                <Route path="/in-progress" element={<InProgress />} />
+                <Route path="/completed" element={<Completed />} />
+                <Route path="/notifications" element={<Notification />} />
+                <Route path="/create-task" element={<OrderCreation />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/order/:orderId" element={<OrderView />} />
+              </Routes>
+            </div>
+          </div>
+        </main>
+        {loadedUserProfile?.is_verified === "False" && (
+          <ActivateAccount token={userToken} email={loadedUserProfile?.email} />
+        )}
       </>
-    )
-  }
+    );
+  };
 
   return (
     <>
       <Routes>
-        <Route path='login' element={<Login/>}/>
-        <Route path='reset-password' element={<PasswordReset/>}/>
-        <Route path='/used-token/:uidb64/:token/' element={<ExpiredToken/>} />
-        <Route path='/bad-token/:uidb64/:token/' element={<BadToken/>} />            
-        <Route path='/set-new-password/:uidb64/:token/' element={<SetPassword/>}/>
-        <Route path='register' element={<Register/> }/>
-        <Route path='/app/*' element={userToken?<Main/>:<Login/>}/>
+        <Route path="login" element={<Login />} />
+        <Route path="reset-password" element={<PasswordReset />} />
+        <Route path="/used-token/:uidb64/:token/" element={<ExpiredToken />} />
+        <Route path="/bad-token/:uidb64/:token/" element={<BadToken />} />
+        <Route
+          path="/set-new-password/:uidb64/:token/"
+          element={<SetPassword />}
+        />
+        <Route path="register" element={<Register />} />
+        <Route path="/app/*" element={userToken ? <Main /> : <Login />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/main/register/Register.jsx
+++ src/pages/main/register/Register.jsx
@@ -1,96 +1,126 @@
-import React from 'react';
-import './register.css';
-import { Link } from 'react-router-dom';
-import { useState, useRef } from 'react';
+import React from "react";
+import "./register.css";
+import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
 import { FiUser } from "react-icons/fi";
 import { MdLock } from "react-icons/md";
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
 import { MdOutlineMail } from "react-icons/md";
-import { useAuthContext } from '../../../providers/AuthProvider';
+import { useAuthContext } from "../../../providers/AuthProvider";
 import PulseLoader from "react-spinners/PulseLoader";
 // import
 const Register = () => {
-    const iconSize = 30;
-    const passwordRef1 = useRef();
-    const passwordRef2 = useRef();
+  const iconSize = 30;
+  const passwordRef1 = useRef();
+  const passwordRef2 = useRef();
 
-    const { loadingReg, handleRegister, successRegister, registerError} = useAuthContext();
+  const { loadingReg, handleRegister, successRegister, registerError } =
+    useAuthContext();
 
-    const [password1, setPassword1] = useState('');
-    const [password2, setPassword2] = useState('');
-    const [visible, setVisible] = useState(false);
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [visible, setVisible] = useState(false);
 
-    
-    const togglePassword = ()=> {
-        setVisible(!visible);
-    }    
+  const togglePassword = () => {
+    setVisible(!visible);
+  };
 
-    return (
-        <div className='register'>
-            <div className='register-info'>
-                <h1>Register with us</h1>
-                <img src="https://img.freepik.com/free-vector/studying-concept-illustration_114360-1301.jpg?w=740&t=st=1699562478~exp=1699563078~hmac=f7e370e5ddf75e11e9d7d411932c595f3642961775fad204b6be5a17ed4f9dbe" alt="" />
-            </div>
-            <div className='register-box'>
-                <form onSubmit={handleRegister} className='register-form'>   
-                    {
-
-                        registerError && registerError.map((error)=>{ 
-                            console.log(error)                               
-                            return error.map((e)=>{
-                                return <li style={{
-                                    color:'orange'
-                                }}>
-                                    {e}
-                                </li>
-                            })                                
-                        })
-                    }                 
-                    <div className='register-content'>
-                        <FiUser className='username-icon' size={iconSize}/>
-                        <input required id='username' type="text" placeholder='Username' />                    
-                    </div>
-                    <div className='register-content'>
-                        <MdOutlineMail className='username-icon' size={iconSize}/>
-                        <input required id='email' type="email" placeholder='email' />                    
-                    </div>
-                    <div className='register-content'>
-                        <MdLock className='password-icon' size={iconSize}/>
-                        <input required id='password1' ref={passwordRef1} value={password1} onChange={(e)=>setPassword1(e.target.value)} type={visible?"text":"password" } placeholder='Password' />                                                  
-                    </div>
-                    <div className='register-content'>
-                        <MdLock className='password-icon' size={iconSize}/>
-                        <input required id='password2' ref={passwordRef2} value={password2} onChange={(e)=>setPassword2(e.target.value)} type={visible?"text":"password" } placeholder='Confirm password' />                                                  
-                        {
-                            visible?
-                            <IoEye onClick={togglePassword} className='password-icon-eye' size={20} />:
-                            <IoEyeOff onClick={togglePassword} className='password-icon-eye' size={20} />                                              
-                        }
-                    </div>
-                    <div className='submit-credentials'>                        
-                        <button>                            
-                             {
-                                loadingReg?
-                                <PulseLoader color='#fff' size={10}  />:
-                                'R E G I S T E R'
-                             }                            
-                        </button>
-                        {/* <button>
+  return (
+    <div className="register">
+      <div className="register-info">
+        <h1>Register with us</h1>
+        <img
+          src="https://img.freepik.com/free-vector/studying-concept-illustration_114360-1301.jpg?w=740&t=st=1699562478~exp=1699563078~hmac=f7e370e5ddf75e11e9d7d411932c595f3642961775fad204b6be5a17ed4f9dbe"
+          alt=""
+        />
+      </div>
+      <div className="register-box">
+        <form onSubmit={handleRegister} className="register-form">
+          {registerError &&
+            registerError.map((error) => {
+              console.log(error);
+              return error.map((e) => {
+                return (
+                  <li
+                    style={{
+                      color: "orange",
+                    }}
+                  >
+                    {e}
+                  </li>
+                );
+              });
+            })}
+          <div className="register-content">
+            <FiUser className="username-icon" size={iconSize} />
+            <input required id="username" type="text" placeholder="Username" />
+          </div>
+          <div className="register-content">
+            <MdOutlineMail className="username-icon" size={iconSize} />
+            <input required id="email" type="email" placeholder="email" />
+          </div>
+          <div className="register-content">
+            <MdLock className="password-icon" size={iconSize} />
+            <input
+              required
+              id="password1"
+              ref={passwordRef1}
+              value={password1}
+              onChange={(e) => setPassword1(e.target.value)}
+              type={visible ? "text" : "password"}
+              placeholder="Password"
+            />
+          </div>
+          <div className="register-content">
+            <MdLock className="password-icon" size={iconSize} />
+            <input
+              required
+              id="password2"
+              ref={passwordRef2}
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
+              type={visible ? "text" : "password"}
+              placeholder="Confirm password"
+            />
+            {visible ? (
+              <IoEye
+                onClick={togglePassword}
+                className="password-icon-eye"
+                size={20}
+              />
+            ) : (
+              <IoEyeOff
+                onClick={togglePassword}
+                className="password-icon-eye"
+                size={20}
+              />
+            )}
+          </div>
+          <div className="submit-credentials">
+            <button>
+              {loadingReg ? (
+                <PulseLoader color="#fff" size={10} />
+              ) : (
+                "R E G I S T E R"
+              )}
+            </button>
+            {/* <button>
                             G O O G L E
                         </button> */}
-                    </div>
-                    <div className='register-prompt'>
-                        <article>Already have an account? 
-                            <span>
-                                <Link to='/login'> Login</Link>
-                            </span>
-                        </article>                        
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
-}
+          </div>
+          <div className="register-prompt">
+            <article>
+              Already have an account?
+              <span>
+                <Link to="/login"> Login</Link>
+              </span>
+            </article>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
