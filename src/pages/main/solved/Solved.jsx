2f8ff2d91d9f705ac66136a6cbbc384f2d98202b
josephmusya <muciajoe@gmail.com>
2024-03-24 01:35:46 +0300

    Add: Email subscription

--- src/pages/main/solved/Solved.jsx
+++ src/pages/main/solved/Solved.jsx
@@ -2,11 +2,35 @@ import React from "react";
 import "./solved.css";
 import { IoCloudDownloadOutline } from "react-icons/io5";
 import { toast } from "react-toastify";
+import { useAuthContext } from "../../../providers/AuthProvider";
 // import OrderComponent from '../../../components/main/order-component/OrderComponent';
 const Solved = () => {
-  const addToWaitList = () => {
-    toast.success("We added you to the wait list");
+  const addToWaitList = async (email) => {
+    const subURL = `${import.meta.env.VITE_API_URL}/subscribe-letter/`;
+    console.log(email);
+    try {
+      const add = await fetch(subURL, {
+        method: "post",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+        }),
+      });
+
+      if (add.ok) {
+        toast.success("We added you to the wait list");
+      } else {
+        toast.warn("We failed to add you. Check again later!");
+      }
+    } catch (error) {
+      toast.error("Failed to add you to list. Try again later!");
+    }
   };
+
+  const { loadedUserProfile } = useAuthContext();
+
   return (
     <div className="solved">
       <div className="coming-soon">
@@ -20,7 +44,9 @@ const Solved = () => {
             published by our expert freelancers
           </article>
         </div>
-        <button onClick={() => addToWaitList()}>Join Wait List</button>
+        <button onClick={() => addToWaitList(loadedUserProfile.email)}>
+          Join Wait List
+        </button>
       </div>
     </div>
   );
